740305745f62bcb40660623b998ee5cfc27c3e4e
Eman <emmanuelgatwech@gmail.com>
2019-11-21 19:28:36 +0300

    Updates

---
+++ precache-manifest.72e84869b9b97bb1d1d0f16241ce1ae8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2229407ec96ace1145276a82759e2b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ae92325002da18b4520",
+    "url": "/static/css/main.64b38d83.chunk.css"
+  },
+  {
+    "revision": "999b4378cfb8a01f00ee",
+    "url": "/static/js/2.bdffad0d.chunk.js"
+  },
+  {
+    "revision": "1ae92325002da18b4520",
+    "url": "/static/js/main.6ec263e1.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
